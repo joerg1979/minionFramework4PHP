7360be2624f43d6be8d2f01e394c05d9e148bab2
seocakes <seocakes@gmail.com>
2015-04-18 22:15:51 +0200

    finished -> klr

--- inc/content/inc.klr.php
+++ inc/content/inc.klr.php
@@ -1,13 +1,7 @@
 <?php 
-var_dump($_POST);
-$zek = 0;
-$lek = $_POST['lek'];
-$lrab = $_POST['lrab'];
-$zek = $lek - ($lek * $lrab / 100);
-
 ?>
 <form action="" method="POST">
-<table border="1">
+    <table border="1" class="col-lg-offset-2 col-md-8 klr">
     <thead>
         <tr>
             <th>Kalkulationsschritt</th>
@@ -17,86 +11,82 @@ $zek = $lek - ($lek * $lrab / 100);
     <tbody>
         <tr>
             <td class="wert">Listeneinkaufspreis </td>
-            <td class="zahlwert"><input type="number" id="lek" name="lek"/>(€)</td>
+            <td class="zahlwert"><input id="lek" name="lek" value="<?php echo ($lek !== "0") ? $lek : '0'; ?>"/>(€)</td>
         </tr>
         <tr>
             <td class="diff">- Lieferrabatt(%)</td>
-            <td class="diffwert"><input type="number" id="lrab" name="lrab"/>(%)</td>
+            <td class="diffwert"><input type="number" id="lrab" name="lrab"  value="<?php echo ($lrab !== "0") ? $lrab : '0'; ?>"/>(%)</td>
         </tr>
         <tr>
             <td class="wert">Zieleinkaufspreis</td>
-            <td class="zahlwert">
-                <?php echo $zek; ?>
-            </td>
+            <td class="zahlwert"><?php echo $zek.' (€)'; ?></td>
         </tr>
         <tr>
             <td class="diff">- Lieferskonto(%)</td>
-            <td class="diffwert"><input name="" />(%)</td>
+            <td class="diffwert"><input type="number" id="lsko" name="lsko"  value="<?php echo ($lsko !== "0") ? $lsko : '0'; ?>"/>(%)</td>
         </tr>
         <tr>
             <td class="wert">Bareinkaufspreis</td>
-            <td class="zahlwert"></td>
+            <td class="zahlwert"><?php echo $barek.' (€)'; ?></td>
         </tr>
         <tr>
             <td class="diff">+ Bezugskosten(€uro)</td>
-            <td class="diffwert"><input name="" />(€)</td>
+            <td class="diffwert"><input id="bezko" name="bezko"  value="<?php echo ($bezko !== "0") ? $bezko : '0'; ?>"/>(€)</td>
         </tr>
         <tr>
             <td class="wert">Bezugspreis</td>
-            <td class="zahlwert"></td>
+            <td class="zahlwert"><?php echo $bezprs.' (€)'; ?></td>
         </tr>
         <tr>
             <td class="diff">+ Handlungskosten / Selbstkosten(%)</td>
-            <td class="diffwert"><input name="" />(%)</td>
+            <td class="diffwert"><input type="number" id="seko" name="seko"  value="<?php echo ($seko !== "0") ? $seko : '0'; ?>"/>(%)</td>
         </tr>
         <tr>
             <td class="wert">Selbstkostenpreis</td>
-            <td class="zahlwert"></td>
+            <td class="zahlwert"><?php echo $seprs.' (€)'; ?></td></td>
         </tr>
         <tr>
             <td class="diff">+ Gewinnzuschlag(%)</td>
-            <td class="diffwert"><input name="" />(%)</td>
+            <td class="diffwert"><input type="number" id="gezu" name="gezu"  value="<?php echo ($gezu !== "0") ? $gezu : '0'; ?>"/>(%)</td>
         </tr>
         <tr>
-            <td class="wert">BAR-Verkaufspreis </td>
-            <td class="zahlwert"></td>
+            <td class="wert">BAR-Verkaufspreis</td>
+            <td class="zahlwert"><?php echo $barvk.' (€)'; ?></td>
         </tr>
         <tr>
             <td class="diff">+Kundenskonto(%)</td>
-            <td class="diffwert"><input name="" />(%)</td>
+            <td class="diffwert"><input type="number" id="kdsko" name="kdsko"  value="<?php echo ($kdsko !== "0") ? $kdsko : '0'; ?>"/>(%)</td>
         </tr>
         <tr>
             <td class="wert">Ziel- Verkaufspreis</td>
-            <td class="zahlwert"></td>
-        </tr>
+            <td class="zahlwert"><?php echo $zvk.' (€)'; ?></td>
         <tr>
             <td class="diff">+Kundenrabatt(%)</td>
-            <td class="diffwert"><input name="" />(%)</td>
+            <td class="diffwert"><input type="number" id="kdrab" name="kdrab"  value="<?php echo ($kdrab !== "0") ? $kdrab : '0'; ?>"/>(%)</td>
         </tr>
         <tr>
             <td class="wert">NETTO- Verkaufspreis</td>
-            <td class="zahlwert"></td>
+            <td class="zahlwert"><?php echo $nvk.' (€)'; ?></td>
         </tr>
         <tr>
             <td class="diff">+Umsatzsteuer(%)</td>
-            <td class="diffwert"><select name="Umsatzsteuer">
-                    <option>19</option>
-                    <option>7</option>
+            <td class="diffwert"><select name="ust" id="ust">
+                   <?php echo ($ust != 7) ? "<option selected='selected'>19</option><option>7</option>" : "<option>19</option><option selected='selected'>7</option>"; ?>"/> 
                 </select>(%)</td>
         </tr>
         <tr>
             <td class="wert">Brutto-Verkaufspreis</td>
-            <td class="zahlwert">
-                
-            </td>
+            <td class="zahlwert"><?php echo $bvk.' (€)'; ?></td>
         </tr>
         <tr>
-            <td></td>
-            <td><input type="submit" value="Ausrechnen" /></td>
-        </tr>
+            <td><input class="btn-block" type="reset" value="Eingaben Zurücksetzen"/></td>
+            <td><input class="btn-block" type="submit" value="Ausrechnen" /></td>
+        </tr></form>
         <tr>
+        <form name="reload" action="" method="GET">
+            <td><input class="btn-block" type="submit" value="Formular leeren"/></td>
             <td></td>
-            <td></td>
+        </form>
         </tr>
         <tr>
             <td></td>
@@ -104,6 +94,3 @@ $zek = $lek - ($lek * $lrab / 100);
         </tr>
     </tbody>
 </table>
-</form>
-
-
--- inc/content/inc.printBody.php
+++ inc/content/inc.printBody.php
@@ -1,3 +1,80 @@
+<?php 
+function check($tocheck) {
+    $checked = htmlspecialchars($tocheck);
+    
+    // @TODO: ändere ,(komma) in .(decimalpoint)
+    if ($checked <= 0 ){
+        $checked = "0";
+    }
+    return $checked;
+}
+
+//function setZielEK($lieferEK, $lieferRab=0) {
+//    // $this->zielEK = $lieferEK - ($lieferEK * $lieferRab / 100); // For OO
+//}
+function getZielEK($lieferEK, $lieferRab=0) {
+    // return $this->zielEK;   // For OO
+    return ($lieferEK - ($lieferEK * $lieferRab / 100));
+}
+function getBarEK($zielEK, $lieferSko=0){
+    return ($zielEK -($zielEK * $lieferSko / 100));
+}
+function getBezugsPreis($barEK, $bezugsKst=0){
+    return ($barEK + $bezugsKst);
+}
+function getSkPreis($bezugsPreis, $skZuschlag=0) {
+    return ($bezugsPreis * (($skZuschlag+100)/100));
+}
+function getBarVK($skPreis, $gewZuschlag=0){
+    return ($skPreis * (($gewZuschlag+100)/100));
+}  
+
+function getZielVK($barVK, $kundenSko=0) {
+    return ($barVK * 100 /(100 - $kundenSko));
+}
+function getNettoVK($zielVK, $kundenRab=0) {
+    return ($zielVK * 100 /(100 - $kundenRab));
+}
+function getBruttoVK($nettoVk, $umsatzSt=19) {
+    return ($nettoVk * ((100+$umsatzSt)/100));
+}
+
+    $lek = $lrab = $zek = "0";
+    $lsko = $barek = "0";
+    $bezko = $bezprs = "0";
+    $seko = $seprs = "0";
+    $gezu = $barvk = "0";
+    $kdsko = $zvk = "0";
+    $kdrab = $nvk = $bvk= "0";
+
+if ($_POST) {
+    $lek    = ($_POST['lek'])   ? check($_POST['lek'])  : "0";
+    $lrab   = ($_POST['lrab'])  ? check($_POST['lrab']) : "0";
+    $zek    = ($lek >= 0)       ? getZielEK($lek, $lrab): "0";
+    
+    $lsko   = ($_POST['lsko'])  ? check($_POST['lsko']) : "0";
+    $barek  = ($zek >= 0)       ? getBarEK($zek, $lsko) : "0" ; 
+    
+    $bezko  = ($_POST['bezko']) ? check($_POST['bezko']): "0";
+    $bezprs = ($bezko >= 0) ? getBezugsPreis($barek, $bezko): "0";
+
+    $seko   = ($_POST['seko'])  ? check($_POST['seko']) : "0";
+    $seprs  = ($seko >= 0)  ? getSkPreis($bezprs, $seko): "0";
+    
+    $gezu   = ($_POST['gezu'])  ? check($_POST['gezu']) : "0"; 
+    $barvk  = ($gezu >= 0)  ? getBarVK($seprs, $gezu) : "0";
+    
+    $kdsko  = ($_POST['kdsko']) ? check($_POST['kdsko']): "0"; 
+    $zvk    = ($kdsko >= 0) ? getZielVK($barvk, $kdsko) : "0";
+    
+    $kdrab  = ($_POST['kdrab']) ? check($_POST['kdrab']): "0";
+    $nvk    = ($kdrab >=0)  ? getNettoVK($zvk, $kdrab)  : "0";
+        
+    $ust    = ($_POST['ust'])   ? check($_POST['ust'])  : "0"; 
+    $bvk    = ($ust >=0)    ? getBruttoVK($nvk, $ust)   : "0";    
+} 
+
+?>
 <!--   *   *   *   *   *   *   *   *   -->
 <!-- Begin CONTAINER cnt02_01 -->
 <div class="container" id="cnt02_01" style="text-align: center; border: 1px black dotted">
@@ -7,14 +84,15 @@
             <article class="col-md-12" style="border: 1px black dashed">
                 <br><hr />
                 <h2>Content Area [Row 1]</h2>
-                <div class="col-md-8" style="text-align: left; height: 550px; width: 95%; border: 1px blue double">
+                <div class="col-md-8" style="text-align: left; height: 650px; width: 95%; border: 1px blue double">
                 
                     <p class="lead text-center alert-warning">Include your BusinesLogic here<em>(inc.printBody.php)</em></p>
-<?php   if($position == ''){
-                              include_once PROJECT__ROOT.'/inc/content/pdo.allUser.php';
-                           } else {
-                               include_once PROJECT_HTTP_ROOT.$position;
-                            }
+                    <?php                   
+                        if($position == ''){
+                            include_once PROJECT_DOCUMENT_ROOT.'/inc/content/pdo.allUser.php';
+                        } else {
+                            include_once PROJECT_DOCUMENT_ROOT.$position;
+                        }
                     ?>
                 </div>                                        
             </article>
